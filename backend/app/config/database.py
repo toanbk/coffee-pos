21d6b01bbc015978a0b4465c295851ad0814e621
Toan Pham <toan@betanet.co.il>
2025-06-04 21:33:54 +0700

    add more DB config

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -11,7 +11,13 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "mysql+pymysql://root:123123@localhost/coffee_pos"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=28000,
+    pool_size=10,
+    max_overflow=20
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
